a8980cb8f1a225e41d66239ac03d0229ed71ac30
N. Petkov <nmpetkov@gmail.com>
2015-03-03 20:10:11 +0200

    Examples

--- userdata/Ztools/scripts/example.php
+++ userdata/Ztools/scripts/example.php
@@ -1,5 +1,17 @@
 <?php
+// Example for use Ztools module for Zikula
+// As you can see, all Zikula environment is available (functions and classes)
 
 echo 'Hello World example!<br /><br />';
 
-echo 'Your username is ' . UserUtil::getVar('uname') . '<br />';
+// Example with your data (Zikula UserUtil)
+echo 'Your username is ' . UserUtil::getVar('uname') . '<br /><br />';
+echo 'And this is all info for you in the site:<br />';
+echo '<pre>';
+    print_r(UserUtil::getVars(UserUtil::getVar('uid')));
+echo '</pre>';
+
+// Example with php functions
+echo '<pre>';
+    print_r(getdate());
+echo '</pre>';
